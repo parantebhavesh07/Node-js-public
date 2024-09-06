3f55c328d15a8fb3d375f48a54a36479afbd233c
Bhavesh Parante <parantebhavesh1154@gmail.com>
2024-09-06 20:29:26 +0530

    make new repo
    make new repo

---
+++ index.js
@@ -0,0 +1,16 @@
+const express = require('express');
+
+const PORT = 5500;
+const app = express();
+
+app.get('/', (req, res) => {
+      res.end("Hello From Home Page Using Express")
+})
+
+app.get('/about', (req, res) => {
+      res.end("Hello From Aboute Page")
+})
+
+app.listen(PORT, () => {
+      console.log(`Server Run On PORT:${PORT}`)
+})
